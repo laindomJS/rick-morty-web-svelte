7709be878a8b70e166427a5c4d9eda28aed6658e
laindomJS <adrijar21@gmail.com>
2023-03-10 23:04:19 -0400

    feat: corrected input styles

--- src/lib/Input.svelte
+++ src/lib/Input.svelte
@@ -3,7 +3,7 @@
 	import SearchIcon from './SearchIcon.svelte';
 </script>
 
-<section class="w-full lg:w-fit my-8 px-4 md:px-10 relative">
+<section class="w-fit my-8 px-4 md:px-10 relative">
 	<input bind:value={$term} class="h-11 p-2 md:w-72 lg:w-96 shadow-md placeholder:italic placeholder:font-semibold text-black border-2 border-green-500" type="text" placeholder="Name of character..." />
 	<SearchIcon />
 </section>
